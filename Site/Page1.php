150dcb96f88bd6b4c615d79c2cdf846e27bdd051
Fernando <n.ando.s@hotmail.com>
2012-11-14 21:30:14 -0200

    adicionando arquivos

---
+++ Site/Page1.php
@@ -0,0 +1,28 @@
+<html>
+<?php
+		$title = "Site.com";
+		$subtitle = "este e um site em PHP";
+?>
+
+	<head>
+		<meta charset="utf-8">	
+		<title> 
+			<?php echo $title . " - " . $subtitle; ?> 
+		</title>
+				
+	</head>
+	
+	<body>
+	
+	<div class="rodape">
+		<p>
+			&reg; copyright Site.com – <?php echo date("Y"); ?> 
+		</p>
+	
+	</div>	
+	
+	</body>
+</html>
+
+
+
