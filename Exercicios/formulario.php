743922fad9087fd63d183877ea9af8f5bfa99b42
Fernando <n.ando.s@hotmail.com>
2012-11-27 19:31:31 -0200

    Adicionandoooo

--- Exercicios/formulario.php
+++ Exercicios/formulario.php
@@ -1,17 +1,81 @@
 <?php
 
-// $_POST - variavel global
 
-// var_dump($_POST); - ver informacoes que vao ser armazenadas
+	
+	echo "Campo Hidden: ". $_POST["escondido"]."<br>";
+	if(!isset($_POST["escondido"]) || ($_POST["escondido"]=="")){
+		echo "O Campo está vazio"."<br>";
+		}
+	$campo1 = strip_tags($_POST["escondido"]);
+	
+	echo "O valor de CAMPO 1 é: ".$_POST["campo1"]."<br>";
+				if(!isset($_POST["campo1"]) || ($_POST["campo1"]=="")){
+		echo "O Campo está vazio"."<br>";
+		}
+	$campo2 = strip_tags($_POST["campo1"]);
+	
+	echo "Senha: ".$_POST["senha"]."<br>";
+			if(!isset($_POST["senha"]) || ($_POST["senha"]=="")){
+		echo "O Campo está vazio"."<br>";
+		}
+	$campo3 = strip_tags($_POST["senha"]);
+	$palavramd5 = md5("senha");
+		echo $palavramd5."<br>"; 
+	
+	
+			echo "Olá " .$_POST["nome"] . "<br>";
+		
+		if(!isset($_POST["nome"]) || ($_POST["nome"]=="")){
+		echo "O Campo está vazio"."<br>";
+		}
+			$campo4 = strip_tags($_POST["nome"]);
+			
+			
+			$email = trim(@$_REQUEST["email"]);
 
-//Se for envaido um post e a variavel nao é vazia entao mostrar os dados
-if(isset($_POST["pagina"]) && !empty($_POST["pagina"] == "contato")){ 
-
-echo $_POST["nome"]."<br>"; //mostrar dados
-echo $_POST["email"]."<br>";
-echo $_POST["mensagem"];
-}else{
-header("Location: Contact.php");
+			if ($email) {
+				if (preg_match ("/^[A-Za-z0-9]+([_.-][A-Za-z0-9]+)*@[A-Za-z0-9]+([_.-][A-Za-z0-9]+)*\\.[A-Za-z0-9]{2,4}$/", $email)) {
+        echo "O e-mail é válido!"."<br>";
+    } else {
+        echo "O e-mail é inválido!"."<br>";
+    }
 }
-//senao redirecionar para a pagina de contato
-?>
+		if(!isset($_POST["email"]) || ($_POST["email"]=="")){
+		echo "O Campo está vazio"."<br>";
+		}
+		$campo5 = strip_tags($_POST["email"]);
+			
+
+			echo "Sua mensagem: ".$_POST["mensagem"]."<br>";
+			if(!isset($_POST["mensagem"]) || ($_POST["mensagem"]=="")){
+		echo "O Campo está vazio"."<br>";
+		}
+		$campo6 = strip_tags($_POST["mensagem"]);
+	
+		if(isset($_POST["numeros"])) 
+	{ echo "Os números de sua preferência são: "."<br>";
+	foreach($_POST["numeros"] as $numero) 
+	{ echo " ".$numero . "<BR>"."<br>"; } 
+	} else { echo "Você não escolheu número preferido!"."<br>"; }
+	
+
+	echo "Seu sistema operacional é: " .$_POST["sistema"]."<br>";
+				if(!isset($_POST["sistema"]) || ($_POST["sistema"]=="")){
+		echo "O Campo está vazio"."<br>";
+		}
+	$campo8 = strip_tags($_POST["sistema"]);
+
+	
+	echo "Seu processador é: " . $_POST["processador"] ."<br>";
+		if(!isset($_POST["processador"]) || ($_POST["processador"]=="")){
+		echo "O Campo está vazio"."<br>";
+		}	
+	$campo9 = strip_tags($_POST["processador"]);
+
+
+			
+		
+		
+
+		
+	?>
